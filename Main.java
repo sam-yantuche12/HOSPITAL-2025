d487756e8d8c4a0474ed695b3ab97b321ab56f7e
samantha <sdyantuche@scl.edu.gt>
2025-05-19 09:05:24 -0600

    proyecto hospital

--- Main.java
+++ Main.java
@@ -1,18 +1,26 @@
-import javax.swing.JFrame; // Para crear ventanas
-import javax.swing.JLabel; // Para mostrar texto en la interfaz
+import javax.swing.*;
+import java.awt.*;
+import vistas.LoginPanel;
 
 public class Main {
+    public static void main(String[] args) {
+        JFrame miVentana = new JFrame();
+        miVentana.setTitle("Hospital SCL");
+        miVentana.setSize(1000, 800);
+        miVentana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        miVentana.setLayout(new BorderLayout());
 
-public static void main(String[] args) {
+        // Banner
+        JPanel panelBanner = new JPanel();
+        panelBanner.setPreferredSize(new Dimension(300, 800));
+        panelBanner.setBackground(Color.RED);
+        miVentana.add(panelBanner, BorderLayout.WEST);
 
-JFrame miVentana = new JFrame(); // Crea un objeto JFrame (ventana)
-miVentana.setTitle("Hospital SCL"); // Establece el título de la ventana
-miVentana.setSize(1000, 800); // Define el tamaño (ancho, alto)
+        // LoginPanel
+        LoginPanel loginPanel = new LoginPanel();
+        miVentana.add(panelBanner, BorderLayout.WEST);
 
-JLabel texto = new JLabel(); // Crea un JLabel (etiqueta de texto)
-texto.setText("Hola mundo"); // Asigna texto a la etiqueta
-miVentana.add(texto); // Añade la etiqueta a la ventana
-
-miVentana.setVisible(true); // Hace visible la ventana
-}
+        // Mostrar
+        miVentana.setVisible(true);
+    }
 }
---
+++ vistas/LoginPanel.java
@@ -0,0 +1,24 @@
+package vistas;
+
+import java.awt.Color;
+import java.awt.Panel;
+
+import javax.swing.JPanel;
+
+public class LoginPanel {
+
+    public panelLogin{
+
+    Public Loginpanel(){
+
+panelLogin.setSize(500, 800);
+panelLogin.setOpaque(true);
+panelLogin.setBackground(Color.PINK);
+
+    }
+    public JPanel getJPanel(){
+        return PanelLogin;
+    }
+    
+}
+}
